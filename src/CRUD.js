fad517183163ec0dc7ab972696dbfd17bfea08b0
04Anshul <anshulgautam04@gmail.com>
2024-03-25 19:11:59 +0530

    first commit

---
+++ src/CRUD.js
@@ -0,0 +1,15 @@
+import React,{useState, useEffect} from "react";
+
+const CRUD = () => {
+    const empdata =[
+        {
+            
+        }
+    ]
+    const [data, setData] = useEffect([]);
+    return(
+        <div>CRUD</div>
+    )
+}
+
+export default CRUD;
